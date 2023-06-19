4316fc12c00b784af780dc9f531fb54e9dfad10d
Anna Smolskaya <anna31rus@mail.ru>
2023-06-20 00:09:01 +0500

    added Portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import './Portfolio.css'
+import './Portfolio.css';
+import arrow from '../../images/portfolio-arrow.svg';
 
 function Portfolio() {
   return (
-    <>
-      Портфолио
-    </>
+    <section className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <div>
+        <ul  className="portfolio__list">
+          <li>
+            <a href="https://github.com/Smolskaia/how-to-learn" className="portfolio__link">
+              <p className="portfolio__text">Статичный сайт</p>
+              <img className="portfolio__arrow-img" src={arrow} alt="стрелка" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Smolskaia/russian-travel" className="portfolio__link">
+              <p className="portfolio__text">Адаптивный сайт</p>
+              <img className="portfolio__arrow-img" src={arrow} alt="стрелка" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Smolskaia/react-mesto-api-full-gha" className="portfolio__link">
+              <p className="portfolio__text">Одностраничное приложение</p>
+              <img className="portfolio__arrow-img" src={arrow} alt="стрелка" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
