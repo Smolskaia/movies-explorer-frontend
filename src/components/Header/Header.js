01b5d9b16cb2d941e6e08dbdbfd685354dea0fa1
Anna Smolskaya <anna31rus@mail.ru>
2023-06-20 18:24:31 +0500

    conditional rendering is implemented in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Link, NavLink } from 'react-router-dom';
 import logo from "../../images/header-logo.svg";
 import account from "../../images/account-image.svg";
@@ -6,24 +7,18 @@ import burger from "../../images/header-burger.svg";
 import "./Header.css";
 
 function Header() {
+  // хук состояния useState используется для реализации условного 
+  // рендеринга в хедере
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // false/true
+
   return (
     <>
-    <div className="header">
+    <div className={`header ${isLoggedIn ? '' : 'header__logged-out'}`}>
       <Link to="/" className="header__logo">
         <img src={logo} alt="Логотип проекта"/>
       </Link>
-      
-      <div className="header__button-wrapper">
-        <Link to="/signup" className="header__button header__button-signup">Регистрация</Link>
-        <Link to="/signin" className="header__button header__button-signin">Войти</Link>
-      </div>
-    </div>
-
-    <div className="header">
-      <Link to="/" className="header__logo">
-        <img src={logo} alt="Логотип проекта"/>
-      </Link>
-      <nav className="header__menu_loged-in">
+      {isLoggedIn ? (
+        <nav className="header__menu_loged-in">
         <div className="header__button-wrapper_loged-in">
           <NavLink to="/movies" className="header__button header__button-movies">Фильмы</NavLink>
           <NavLink to="/saved-movies" className="header__button header__button-saved-movies">Сохранённые фильмы</NavLink>
@@ -39,6 +34,12 @@ function Header() {
           </button>
         </div>
       </nav>
+      ) : (
+        <div className="header__button-wrapper">
+        <Link to="/signup" className="header__button header__button-signup">Регистрация</Link>
+        <Link to="/signin" className="header__button header__button-signin">Войти</Link>
+      </div>
+      )}
     </div>
     </>
   );
