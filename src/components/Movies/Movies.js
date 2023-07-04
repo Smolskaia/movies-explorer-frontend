616d6190270a32d9cf1bd647c059bae735ee634b
Anna Smolskaya <anna31rus@mail.ru>
2023-07-04 19:30:20 +0500

    added Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
 import SearchFilmForm from '../SearchFilmForm/SearchFilmForm';
@@ -6,8 +6,18 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
 import { cardsList } from '../../utils/constants';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import Preloader from "../Preloader/Preloader";
 
 function Movies() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Имитируем загрузку данных в течение 2 секунд
+    setTimeout(() => {
+      setIsLoading(false); // прелоадер в false через 2 секунды
+    }, 2000);
+  }, []);
 
   return (
     <section className="movies">
@@ -17,9 +27,11 @@ function Movies() {
       />
       <main>
         <SearchFilmForm/>
+        {isLoading ? <Preloader /> : null}
         <MoviesCardList
-        cards={cardsList}
-        isSavedMoviesPage={false}
+          cards={cardsList}
+          isSavedMoviesPage={false}
+        />
         />
       </main>
       <Footer />
