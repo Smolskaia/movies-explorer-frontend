e706d6f421b0e0faea20323d97abd506e8e80e0f
Anna Smolskaya <anna31rus@mail.ru>
2023-07-13 00:09:16 +0500

    set resize and showMore button

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard.js";
 
@@ -9,16 +9,54 @@ function MoviesCardList(props) {
     isSavedMoviesPage } = props; 
 
   const [isSavedArray, setIsSavedArray] = useState(cards.map(() => false));
-  const [visibleCardsCount, setVisibleCardsCount] = useState(12);
+  const [visibleCardsCount, setVisibleCardsCount] = useState(getVisibleCardsCount());
 
-  const handleSaveClick = (index) => {
+  function handleSaveClick(index) {
     const updatedArray = [...isSavedArray];
     updatedArray[index] = !updatedArray[index];
     setIsSavedArray(updatedArray);
   };
 
+  function getVisibleCardsCount() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1280) {
+      return 12;
+    } else if (screenWidth >= 768) {
+      return 8;
+    } else if (screenWidth >= 320) {
+      return 5;
+    }
+    return 0;
+  };
+
+  function handleResize() {
+    setTimeout(() => {
+      setVisibleCardsCount(getVisibleCardsCount());
+    }, 200); // Задержка в 200 миллисекунд
+  };
+
+  useEffect(() => {
+    setVisibleCardsCount(getVisibleCardsCount());
+    setIsSavedArray(cards.map(() => false));
+// Чтобы колбэк-функция слушателя не срабатывала слишком часто, например, 
+//при изменении ширины экрана в отладчике, мы рекомендуем установить setTimeout 
+// на вызов этой функции внутри слушателя "resize".
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  
   const handleShowMoreClick = () => {
-    setVisibleCardsCount((prevCount) => prevCount + 12);
+    if (visibleCardsCount < cards.length) {
+      if (window.innerWidth >= 879) {
+        setVisibleCardsCount((prevCount) => prevCount + 3);
+      } else if (window.innerWidth >= 560) {
+        setVisibleCardsCount((prevCount) => prevCount + 2);
+      } else if (window.innerWidth >= 320) {
+        setVisibleCardsCount((prevCount) => prevCount + 2);
+      }
+    }
   };
 
   const visibleCards = cards.slice(0, visibleCardsCount);
