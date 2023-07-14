52a51efa84831bcfa08250b049b8f4b22e3aba32
Anna Smolskaya <anna31rus@mail.ru>
2023-07-14 19:26:00 +0500

    created disabled submit button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,29 +3,52 @@
 имеют общее содержимое:  
 контейнер с содержимым, логотип, форма(заголовок и инпуты), 
 кнопка сабмит и строка со ссылкой под кнопкой
-*/ 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Form.css';
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Form.css";
 import logo from "../../images/header-logo.svg";
 
 function Form(props) {
-  const { title, children, buttonText, question, linkText, link } = props
+  const {
+    title,
+    children,
+    buttonText,
+    question,
+    linkText,
+    link,
+    isLoading,
+    isDisabled,
+    onSubmit,
+  } = props;
   return (
     <div className="form">
-      <Link to="/" className="form__logo">
-        <img src={logo} alt="логотип" />
+      <Link
+        to="/"
+        className="form__logo"
+      >
+        <img
+          src={logo}
+          alt="логотип"
+        />
       </Link>
       <h3 className="form__title">{title}</h3>
-      <form className="form__container">
+      <form className="form__container" onSubmit={onSubmit} noValidate>
         {children}
-        <button type="submit" className="form__btn-save">
+        <button
+          type="submit"
+          className={`form__btn-save ${isDisabled || isLoading ? '' : 'form__btn-save_disabled'}`
+        }
+        >
           {buttonText}
         </button>
       </form>
       <span className="form__text">
         {question}
-        <Link to={link} className="form__link">
+        <Link
+          to={link}
+          className="form__link"
+        >
           {linkText}
         </Link>
       </span>
